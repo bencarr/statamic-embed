852aaff5ddeb363a89bfadee56e9b0e35f6ff459
Ben Carr <ben.carr@me.com>
2022-06-09 18:49:01 -0400

    Adjust loading indicator to bar style

--- resources/js/components/fieldtypes/Embed.vue
+++ resources/js/components/fieldtypes/Embed.vue
@@ -2,24 +2,31 @@
 	<div class="space-y-1">
 		<div class="relative">
 			<text-input type="url" v-model="buffer" placeholder="https://" :classes="{ 'pr-4': fetching }"/>
-			<div class="absolute top-0 right-0 p-1" v-if="fetching">
-				<div class="embed-preview-spinner"></div>
-			</div>
 		</div>
 
-		<div class="relative bg-grey-20 border rounded mt-2 overflow-hidden" v-if="url">
+		<div class="relative rounded mt-2 overflow-hidden" v-if="url">
+			<transition
+				name="test"
+				enter-class="transition-opacity duration-150"
+				enter-active-class="opacity-100"
+				leave-class="transition-opacity duration-300"
+				leave-active-class="opacity-0"
+			>
+				<div class="z-10 embed-loading-bar rounded-t absolute top-0 left-0 w-full h-2" v-if="fetching"></div>
+			</transition>
 			<iframe
 				:src="previewUrl"
 				@load="onPreviewLoad"
-				class="embed-preview-frame"
-				:class="{ 'opacity-50 transition': fetching }"
+				class="w-full bg-grey-20 border transition"
+				height="315"
+				:class="{ 'opacity-50': fetching }"
 			></iframe>
 		</div>
 	</div>
 </template>
 
 <script>
-import qs from 'qs';
+import qs from 'qs'
 
 export default {
 	mixins: [Fieldtype],
@@ -40,12 +47,12 @@ export default {
 
 	computed: {
 		previewUrl() {
-			if (!this.url) return null;
+			if (!this.url) return null
 
-			return cp_url('embed-fieldtype/preview', { url: this.url }) + '?' + qs.stringify({ url: encodeURIComponent(this.url) })
+			return cp_url('embed-fieldtype/preview', {url: this.url}) + '?' + qs.stringify({url: encodeURIComponent(this.url)})
 		},
 		code() {
-			return _.get(this, ['preview', 'code', 'html']);
+			return _.get(this, ['preview', 'code', 'html'])
 		},
 	},
 
@@ -63,7 +70,7 @@ export default {
 		// 		.finally(() => this.fetching = false)
 		// },
 		onPreviewLoad() {
-			this.fetching = false;
+			this.fetching = false
 		},
 	},
 	watch: {
@@ -82,11 +89,11 @@ export default {
 			// doesn't think the field was edited
 			if (this.metaChanging) return
 
-			this.fetching = true;
+			this.fetching = true
 
 			this.updateDebounced(this.url)
 		},
-		buffer: _.debounce(function(newValue) {
+		buffer: _.debounce(function (newValue) {
 			console.log('debounced')
 
 			this.url = newValue
@@ -98,23 +105,30 @@ export default {
 }
 </script>
 <style>
-.embed-preview-frame {
+.embed-loading-bar {
+	position: relative;
+	overflow: hidden;
+	background: rgba(255, 38, 158);
+}
+
+.embed-loading-bar::after {
+	content: '';
+	position: absolute;
+	height: 100%;
 	width: 100%;
-	height: 315px;
+	background: linear-gradient(to right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1) 25%, rgba(255, 255, 255, 0));
+	opacity: 0.75;
+	animation: progress 1.5s infinite ease-in-out;
 }
 
-.embed-preview-spinner {
-	display: inline-block;
-	border: 3px solid rgba(0, 0, 0, 0.15);
-	border-radius: 50%;
-	width: 1.25rem;
-	height: 1.25rem;
-	border-top-color: rgba(0, 0, 0, 0.5);
-	animation: spin calc(1s) linear infinite;
+@-webkit-keyframes progress {
+	0% {
+		left: 0;
+		transform: translateX(-100%);
+	}
+	100% {
+		left: 100%;
+		transform: translateX(0%);
+	}
 }
-
-@keyframes spin {
-	 0% { transform: rotate(0deg); }
-	 100% { transform: rotate(360deg); }
- }
 </style>
