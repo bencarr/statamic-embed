3ba15f49930e646748456cfe9d57886bf7fefc51
Ben Carr <ben.carr@me.com>
2022-06-24 17:38:30 -0400

    Add default HTML output for embeds and images

--- src/Helpers/EmbedData.php
+++ src/Helpers/EmbedData.php
@@ -2,6 +2,7 @@
 
 namespace BenCarr\Embed\Helpers;
 
+use BenCarr\Embed\Actions\EmbedManager;
 use BenCarr\Embed\Concerns\HasChainableAttributes;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Contracts\Support\Htmlable;
@@ -10,17 +11,32 @@ class EmbedData implements Arrayable, Htmlable
 {
     use HasChainableAttributes;
 
+    public EmbedCache $cache;
+
     public function __construct(
         public string $url,
         public bool $preview = true,
     ) {
         $this->initChainableAttributes();
+        $this->cache = EmbedManager::url($url)->get();
     }
 
     public function toHtml()
     {
+        if ($code = $this->cache->code) {
+            return $code->html;
+        }
+
+        if ($image = $this->cache->image) {
+            return sprintf('<img %s />', $this->attributes->merge([
+                'src' => $image,
+                'alt' => $this->cache->description,
+            ]));
+        }
+
         return sprintf('<a %s>%s</a>', $this->attributes->merge([
             'href' => $this->url,
+            'target' => '_blank',
         ]), $this->url);
     }
 
@@ -31,8 +47,6 @@ class EmbedData implements Arrayable, Htmlable
 
     public function toArray(): array
     {
-        return [
-            'url' => $this->url,
-        ];
+        return $this->cache->toArray();
     }
 }
