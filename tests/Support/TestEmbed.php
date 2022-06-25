1b814f95eb1a1dc2fe16fe31bfd8b773379ec0e3
Ben Carr <ben.carr@me.com>
2022-06-24 21:43:48 -0400

    Fix TextEmbed schema

--- tests/Support/TestEmbed.php
+++ tests/Support/TestEmbed.php
@@ -9,13 +9,20 @@ class TestEmbed implements Arrayable
 {
     public function __construct(
         public string $url = 'url',
+        public bool $preview = true,
     ) {
+    }
+
+    public static function url(string $url)
+    {
+        return new self($url);
     }
 
     public function toArray(): array
     {
         return [
             'url' => $this->url,
+            'preview' => $this->preview,
         ];
     }
 
